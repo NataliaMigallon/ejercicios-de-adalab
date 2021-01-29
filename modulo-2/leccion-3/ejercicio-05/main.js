b85b775d44cc51cffdcc678e6a80ead13bf8c9f5
NataliaMigallon <natmigallon@gmail.com>
2021-01-29 17:21:41 +0100

    Add exercise 5 rainbow

--- modulo-2/leccion-3/ejercicio-05/main.js
+++ modulo-2/leccion-3/ejercicio-05/main.js
@@ -1,7 +1,28 @@
 'use strict';
 
-function getEl(a) {
-  const value = document.querySelector(a);
-  return value
+const rainbow = document.querySelector('.notification');
+const title = document.querySelector('.title');
+const text = document.querySelector('.text');
+
+if (rainbow.classList.contains('success')) {
+  title.innerHTML = 'Correcto';
+  text.innerHTML = 'Los datos son correctos';
+} else if (rainbow.classList.contains('error')) {
+  title.innerHTML = 'Error';
+  text.innerHTML = 'Ha surgido un error';
+} else if (rainbow.classList.contains('warning')) {
+  title.innerHTML = 'Aviso';
+  text.innerHTML = 'Tenga cuidado';
 }
-const btnEl = getEl('.btn');
+
+rainbow.classList.add('warning');
+
+/* if (rainbow.classList.contains('success')) {
+  rainbow.innerHTML = '<h1>Correcto</h1><p>Los datos son correctos</p>';
+} else if (rainbow.classList.contains('error')) {
+  rainbow.innerHTML = '<h1>Error</h1><p>Ha surgido un error</p>';
+} else if (rainbow.classList.contains('warning')) {
+  rainbow.innerHTML = '<h1>Aviso</h1><p>Tenga cuidado</p>';
+}
+
+rainbow.classList.add('success'); */
