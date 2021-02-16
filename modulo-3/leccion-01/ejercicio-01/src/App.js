9dce7ca6e484bbe32eabb9443cc14dbe034f8f11
NataliaMigallon <natmigallon@gmail.com>
2021-02-16 11:13:12 +0100

    Add exercise 1

---
+++ modulo-3/leccion-01/ejercicio-01/src/App.js
@@ -0,0 +1,54 @@
+/* import logo from "./logo.svg"; */
+import profile_photo from "./profile_photo.JPG";
+import "./App.css";
+
+function App() {
+  const footer = (
+    <footer className="App-footer">
+      <div className="container_footer">
+        <p className="read_more">Leer más...</p>
+        <span className="likes">37</span>
+        <i class="heart_icon fa fa-heart" aria-hidden="true"></i>
+      </div>
+    </footer>
+  );
+  const header = (
+    <header className="App-header">
+      <div className="container">
+        <div>
+          <img
+            src={profile_photo}
+            className="profile_photo"
+            alt="profile_photo"
+          />
+        </div>
+        <div className="profile_info">
+          <h1 className="profile_name">Natalia Migallón</h1>
+          <h2 className="date">Lunes 15 de febrero de 2021</h2>
+        </div>
+      </div>
+    </header>
+  );
+  const main = (
+    <main className="App-main">
+      <p className="text">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod
+        tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea
+        commodi consequat. Quis aute iure reprehenderit in voluptate velit esse
+        cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat
+        cupiditat non proident, sunt in culpa qui officia deserunt mollit anim
+        id est laborum.
+      </p>
+    </main>
+  );
+  return (
+    <div className="App">
+      {header}
+      {main}
+      {footer}
+    </div>
+  );
+}
+
+export default App;
