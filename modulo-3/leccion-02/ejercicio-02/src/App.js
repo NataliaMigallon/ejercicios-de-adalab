cc4ee7cc4e59cfe470c4ec765633c06a728e96cd
NataliaMigallon <natmigallon@gmail.com>
2021-02-17 11:33:49 +0100

    Add exercise props in MediaCard

--- modulo-3/leccion-02/ejercicio-02/src/App.js
+++ modulo-3/leccion-02/ejercicio-02/src/App.js
@@ -3,7 +3,11 @@ import MediaCard from "./MediaCard";
 
 class App extends React.Component {
   render() {
-    return <MediaCard />;
+    return (
+      <div>
+        <MediaCard name="Ana Migallón" date="Martes 16 de febrero de 2021" likes="40"/>
+      </div>
+    );
   }
 }
 export default App;
